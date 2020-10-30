2913a9199585659838aef76a4f008b8683751c29
Justin Ong <justin.ong.97@gmail.com>
2020-10-30 14:36:11 +0800

    Remove duplicate Timetable class

--- src/main/java/mystars/entities/timetable.java
+++ src/main/java/mystars/entities/timetable.java
@@ -1,44 +1,29 @@
-package mystars.entities;
-/**
- * <h1>Class: timetable</h1>
- * 
- * This timetable class stores an array of indexes for a student's registered courses.
- * 
- * @param indexes An arraylist of string elements corresponding to the courses that
- * a student has registered for. 
- */
-
 import java.util.ArrayList;
 
-public class timetable
-{
-	//An arraylist stores a dynamicaly sized collection of elements.
-	//It can resize to accomodate new elements or shrink when elements are removed.
-	private ArrayList<String> indexes = new ArrayList<String>();
+public class Timetable {
 	
-	public timetable()
-	{
-		
+	private ArrayList<Index> indexes;
+	
+	public void addIndex(Course course, int indexNo) {
+		try {
+			indexes.add(course, indexNo);
+		} catch(Exception e) {
+			System.out.println("Index does not exist.");
+		}
 	}
 	
-	/**
-	 * This method adds a course and index to a student's timetable.
-	 * 
-	 * @param course The enum of the course to be added to the student's timetable.
-	 * @param indexNo The index number of the course to be added to the student's timetable.
-	 */
-	public void addIndex(Course course, int indexNo)
-	{
-		indexes.add(indexNo, course);
+	public void removeIndex(int indexNo) {
+		try {
+			indexes.remove(indexNo);
+		} catch(Exception e){
+			System.out.println("Index not found.");
+		}
 	}
 	
-	/**
-	 * This method removes a course's index from the student's timetable.
-	 * 
-	 * @param indexNo The index number of the course to be removed from the student's timetable.
-	 */
-	public void removeIndex(int indexNo)
-	{
-		indexes.remove(indexNo);
+	public void checkClash(Lesson lessonA, Lesson lessonB) {
+		if (lessonA.week == lessonB.week && lessonA.day==lessonB.day && lessonA.period == lessonB.period) {
+			System.out.println("There is a clash between " + lessonA.index + " and " + lessonB.index);			
+		}		
 	}
+	
 }
