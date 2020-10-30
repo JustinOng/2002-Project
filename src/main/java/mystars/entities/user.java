843566e1392f30a3aab1355e939696909d36206d
Benedict Lee <70098630+benedictlee21@users.noreply.github.com>
2020-10-30 14:13:11 +0800

    Inserted SHA-256 hashing functions

--- src/main/java/mystars/entities/user.java
+++ src/main/java/mystars/entities/user.java
@@ -18,6 +18,11 @@ package mystars.entities;
  * names and their password hashes.  
  */
 
+import java.math.BigInteger;  
+import java.nio.charset.StandardCharsets; 
+import java.security.MessageDigest;  
+import java.security.NoSuchAlgorithmException;  
+
 import java.util.HashMap;
 
 public class user
@@ -42,15 +47,57 @@ public class user
 		
 	}
 	
+	/*
+	 * ------------------------------------------------------
+	 * Additional functions for calculating password hash.
+	 */
+	
+	public static byte[] getSHA(String input) throws NoSuchAlgorithmException 
+    {  
+        // Static getInstance method is called with hashing SHA  
+        MessageDigest md = MessageDigest.getInstance("SHA-256");  
+  
+        // digest() method called  
+        // to calculate message digest of an input  
+        // and return array of byte 
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));  
+    }
+	
+	public static String toHexString(byte[] hash) 
+    { 
+        // Convert byte array into signum representation  
+        BigInteger number = new BigInteger(1, hash);  
+  
+        // Convert message digest into hex value  
+        StringBuilder hexString = new StringBuilder(number.toString(16));  
+  
+        // Pad with leading zeros 
+        while (hexString.length() < 32)  
+        {  
+            hexString.insert(0, '0');  
+        }  
+        return hexString.toString();  
+    }
+	
 	/**
-	 * This method calculates the hash of the user's password.
+	 * This method calculates the hash of the user's password
+	 * using the SHA-256 algorithm.
 	 * 
 	 * @param input The user's plain text password.
-	 * @return
+	 * @return The SHA-256 hash of the user's password.
 	 */
 	protected String hashPassword(String input)
 	{
-		
+		//Try to compute the hash.
+		try 
+        {  
+            return toHexString(getSHA(input));
+        } 
+        //If the hash calculation fails.
+        catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception thrown for incorrect algorithm: " + e);  
+        }
+		return "Hash not successfully generated.";
 	}
 	
 	/**
