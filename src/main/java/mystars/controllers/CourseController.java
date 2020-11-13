cb3343aa1dc2ee7725b571113cc3dde12b410a84
CanonAAA <whseah@hotmail.com>
2020-11-13 11:37:59 +0800

    Java_Doc
    Updated courseController & Course for java.doc.

--- src/main/java/mystars/controllers/CourseController.java
+++ src/main/java/mystars/controllers/CourseController.java
@@ -7,29 +7,57 @@ import java.util.HashMap;
 import mystars.entities.*;
 import mystars.exceptions.AppException;
 
-/* 1) courseManger will do all the condition and logic checking?
- * 2) Course is to just retrieve the date from the other classes?
- * 3) Both Course.java and courseManager have createCourse, createIndex etc, who will do the instantiate the object reference?
- * 4) Got many same functions in Couse.java and courseManger, eg registerCourse, dropCourse etc... who call who?
- * 5) Ask the enum everything in Course.java? 
+/**
+ * <h1>Class: courseController</h1>
  * 
+ * This courseController class manages the creation of course, course index and lesson. 
+ * In addition, it manages registration of course and dropping of course.
+ * It also manages the changing of index and swopping of index.
  * 
- * 
- * 
- * */
+ */
 
 public class CourseController {
+	/**
+	 * This method creates a new course.
+	 * 
+	 * @param name 		    The course's name.
+	 * @param courseCode    The course's code.
+	 * @param school 	    The course's school.
+	 * @throws AppException If courseCode already exist.
+	 */
 
-	public void createCourse(String name, String courseCode, School School) throws AppException {
-		Course course = new Course(name, courseCode, School);
+	public void createCourse(String name, String courseCode, School school) throws AppException {
+		Course course = new Course(name, courseCode, school);
 	}
+	/**
+	 * This method creates a new index.
+	 * 
+	 * @param courseCode    The index course's code.
+	 * @param indexNo       The index no.
+	 * @param maxEnrolled   The max enrolled of the course index.
+	 * @throws AppException If courseCode is not found.
+	 */
 
 	public void createIndex(String courseCode, int indexNo, int maxEnrolled) throws AppException {
 		Course c = Course.getCourse(courseCode);
 		c.createIndex(indexNo, maxEnrolled);
 	}
+	/**
+	 * This method creates a new lesson.
+	 * 
+	 * @param courseCode    The lesson's course code.
+	 * @param indexNo       The lesson's index no.
+	 * @param lessonType    The lesson type of the lesson.
+	 * @param day           The lesson's day.
+	 * @param Location      The lesson's location.
+	 * @param groupNo       The lesson's group number.
+	 * @param week  	    The lesson's week.
+	 * @param startPeriod   The lesson's start period.
+	 * @param endPeriod	    The lesson's end period.
+	 * @throws AppException If courseCode is not found.
+	 * @throws AppException If indexNo is not found.
+	 */
 
-// Unsure about the parameters in the function are in the correct format
 	public void createLesson(String courseCode, int indexNo, LessonType lessonType, Day day, String location,
 			String groupNo, boolean[] week, int startPeriod, int endPeriod) throws AppException {
 		Course c = Course.getCourse(courseCode);
@@ -42,7 +70,16 @@ public class CourseController {
 		student.getTimetable().addIndex(i);
 		c.register(student, indexNo);
 	}
-
+	/**
+	 * This method register the student to a course.
+	 * 
+	 * @param student The student's.
+	 * @param indexNo The index no the student wants to register.
+	 * @throws AppException If indexNo is not found.
+	 * @throws AppException If index has already been registered.
+	 * @throws AppException If index clashes with another index.
+	 * @throws AppException If indexNo of the student has already been registered.
+	 */
 	public void registerCourse(Student student, int indexNo) throws AppException {
 		Index index = Index.getIndex(indexNo);
 		student.getTimetable().assertAddIndex(index);
@@ -50,13 +87,34 @@ public class CourseController {
 		index.getCourse().register(student, indexNo);
 		student.getTimetable().addIndex(index);
 	}
-
+	/**
+	 * This method drop the student from a course.
+	 * 
+	 * @param student 	 	The student who wants to drop the course.
+	 * @param courseCode 	The course that the student wants to drop.
+	 * @throws AppException If courseCode already exist.
+	 * @throws AppException If student is not enrolled in the course.
+	 *  
+	 */
 	public void dropCourse(Student student, String courseCode) throws AppException {
 		Course course = Course.getCourse(courseCode);
 		Index index = course.drop(student);
 		student.getTimetable().removeIndex(index);
 	}
-
+	/**
+	 * This method changes the student's index from the current index 
+	 * to the targeted index of targetIndexNo
+	 * 
+	 * @param courseCode    The course's code.
+	 * @param student       The student that wants to change index.
+	 * @param targetIndexNo The index number that the student wants to change to.
+	 * @throws AppException If courseCode is not found.
+	 * @throws AppException If student is not enrolled in the course.
+	 * @throws AppException If targetIndexNo is not found.
+	 * @throws AppException If student's timetable cannot accommodate newIndex after removing curIndex. 
+	 * @throws AppException If targetIndexNo of the student has no more vacancies.
+	 * 
+	 */
 	public void changeIndex(String courseCode, Student student, int targetIndexNo) throws AppException {
 		Course course = Course.getCourse(courseCode);
 		Index curIndex = course.getStudentIndex(student);
@@ -67,6 +125,24 @@ public class CourseController {
 		student.getTimetable().removeIndex(curIndex);
 		student.getTimetable().addIndex(newIndex);
 	}
+	/**
+	 * This method allow the student to swop the course index with another student.
+	 * 
+	 * @param studentA      The studentA's.
+	 * @param indexNoA      The studentA's index.
+	 * @param studentB 	    The studentB's.
+	 * @param indexNoB      The studentB's index.
+	 * @throws AppException If studentA is studentB.
+	 * @throws AppException If indexNoA is not found.
+	 * @throws AppException If indexNoB is not found.
+	 * @throws AppException If indexA.getCourse() is not same as indexB.getCourse()
+	 * @throws AppException If studentA's timetable cannot accommodate indexNoB after removing indexA.
+	 * @throws AppException If studentB's timetable cannot accommodate indexNoA after removing indexB.
+	 * @throws AppException If indexNoA and indexNoB is the same index.
+	 * @throws AppException If studentA is not enrolled in indexNoA.
+	 * @throws AppException If studentB is not enrolled in indexNoB.
+	 * 
+	 */
 
 	public void swopIndex(Student studentA, int indexNoA, Student studentB, int indexNoB)
 			throws AppException {
@@ -88,6 +164,7 @@ public class CourseController {
 		studentB.getTimetable().addIndex(indexA);
 	}
 
+	
 	public HashMap<String, String> getRegisteredInfo(Student student) {
 		HashMap<String, String> data = new HashMap<String, String>();
 
@@ -97,7 +174,7 @@ public class CourseController {
 
 		return data;
 	}
-	
+
 	public HashMap<String, Integer> getIndexInfo(String courseCode) throws AppException {
 		HashMap<String, Integer> data = new HashMap<String, Integer>();
 		
--- src/main/java/mystars/entities/Course.java
+++ src/main/java/mystars/entities/Course.java
@@ -6,16 +6,50 @@ import java.util.HashMap;
 import mystars.enums.*;
 import mystars.exceptions.AppException;
 
+/**
+ *<h1>Class: Course</h1>
+ *
+ *
+ *This course class manages the logic courseController. 
+ *It performs all the function that are required for courseController.
+ *It inherits from the Entity Class.
+ *
+ */
 public class Course extends Entity {
+	/**
+	*Serialization of the course ID
+	*/
 	private static final long serialVersionUID = 1L;
 	
+	/**
+	*The name of the course.
+	*/
 	protected String name;
+	
+	/**
+	*The course code of the course.
+	*/
 	protected String courseCode;
+	
+	/**
+	*The school that the course belongs to.
+	*/
 	protected School school;
-
+	
+	/**
+	*A hash map of Index according to the index number in the hash Map.
+	*/
 	protected HashMap<Integer, Index> indexes = new HashMap<Integer, Index>();
 
-	// constructor of Course
+	/**
+	 *Constructor for Course
+	 *This class is responsible for the creation of a course object.
+	 * 
+	 * @param name        	The Course's name.
+	 * @param courseCode    The Course Code.
+	 * @param school		The Course's school.
+	 * @throws AppException	If the courseCode already existed.
+	 */
 	public Course(String name, String courseCode, School school) throws AppException {
 		if (get("course", courseCode) != null) {
 			throw new AppException(String.format("%s already exists", courseCode));
@@ -27,7 +61,13 @@ public class Course extends Entity {
 		store("course", courseCode, this);
 	}
 
-// get the course according to the course code
+	/**
+   	* Gets Course from a course code input
+   	* 
+   	* @param courseCode 	This parameter is the course's course code.
+   	* @return Course		This returns the Course that corresponds the course code input.
+	* @throws AppException 	If the courseCode already exist.
+   	*/
 	public static Course getCourse(String courseCode) throws AppException {
 		Course course = (Course) get("course", courseCode);
 		if (course != null) {
@@ -37,27 +77,48 @@ public class Course extends Entity {
 		throw new AppException(String.format("%s is not a valid course code", courseCode));
 	}
 
-	// accessor method to get the name of the class course
+	/**
+   	* Gets Course's name from a courseCode input.
+   	* @return name 	This returns the Course's name.
+   	*/
 	public String getName() {
 		return this.name;
 	}
 
-	// accessor method to get the course code of the class course
+	/**
+   	* Gets Course code from a courseCode input.
+   	* @return courseCode This returns the courseCode.
+   	*/
 	public String getCourseCode() {
 		return this.courseCode;
 	}
-
-	// accessor method to get the school of the class course
+	
+	/**
+   	* Gets the Course's School
+   	* @return school This returns the Course's school.
+   	*/
 	public School getSchool() {
 		return this.school;
 	}
 
+	/**
+   	* Creates a new Index with the given index number and maximum enrolled number of students
+   	* and place it into the hash map for indexes
+   	* @param indexNo 	The new Index's Index number
+	* @param maxEnrolled 	The maximum enrolled students allowed for the new Index
+   	*/
 	public void createIndex(int indexNo, int maxEnrolled) throws AppException {
 		// ask why does create index only have 2 parameter but the index class have 6
 		Index index = new Index(this, indexNo, maxEnrolled);
 		indexes.put(indexNo, index);
 	}
 
+	/**
+   	* Gets the index object from this course if the index of the course exist
+   	* @param indexNo 		The Index's index number
+	* @return 				The object index according to the index number
+	* @throws AppException	If the index number is not found for the index
+   	*/
 	public Index getIndex(int indexNo) throws AppException {
 		if (indexes.containsKey(indexNo)) {
 			return indexes.get(indexNo);
@@ -66,6 +127,18 @@ public class Course extends Entity {
 		throw new AppException(String.format("Index %d was not found", indexNo));
 	}
 
+	/**
+   	* Creates a new lesson 
+   	* @param indexNo 		The index number that the lesson belongs to
+	* @param lessonType		The lesson's lesson type
+	* @param day			Which day the lesson is on
+	* @param location		The location the lesson is at
+	* @param groupNo		The group number the lesson belongs to
+	* @param week			Whether the lesson falls on odd or even
+	* @param startPeriod	The starting time when the lesson begins
+	* @param endPeriod		The ending time when the lesson begins
+	* @throws AppException 	if the index number is not found in the index
+   	*/
 	public void createLesson(int indexNo, LessonType lessonType, Day day, String location, String groupNo,
 			boolean[] week, int startPeriod, int endPeriod) throws AppException {
 		// no String courseCode i think
@@ -73,11 +146,14 @@ public class Course extends Entity {
 		index.createLesson(lessonType, day, location, groupNo, week, startPeriod, endPeriod);
 	}
 
+	/**
+	* Getting all of the students that registered in the course 
+	* @return studentList 	This method returns all of the students that registered in the course as a new ArrayList
+	*/
 	public ArrayList<Student> getStudentList() {
-		// to create an array of all the students in the course
+
 		ArrayList<Student> studentList = new ArrayList<Student>();
 
-		// adding all the new students to the array list
 		for (Index i : indexes.values()) {
 			studentList.addAll(i.getStudentList());
 		}
@@ -85,19 +161,34 @@ public class Course extends Entity {
 		return studentList;
 	}
 	
+	/** 
+	* Gets all the list of indexes that is from the course
+	* @return A new Array List of indexes from the course
+	*/
 	public ArrayList<Index> getIndexes() {
 		return new ArrayList<Index>(indexes.values());
 	}
 
+	/** 
+	* Check if the student is in the index
+	* @param student	The student that you want check with
+	* @return the index if the student is in the index
+	* @return null if the student is not in the index
+	*/
 	public Index findStudent(Student student) {
 		for (Index i : indexes.values()) {
 			if (i.hasStudent(student))
 				return i;
 		}
-
 		return null;
 	}
 
+	/** 
+	* Gets the index that the student is in
+	* @param student	The student that you want check with
+	* @return the index if the student is in the index
+	* @throws AppException if the student is not enrolled in the course
+	*/
 	public Index getStudentIndex(Student student) throws AppException {
 		Index index = findStudent(student);
 		
@@ -107,29 +198,45 @@ public class Course extends Entity {
 		return index;
 	}
 
+	/** 
+	* Registers the student in the target index
+	* @param student	The student to be registered in the index
+	* @param indexNo	The index that the student is to be registered to
+	* @throws AppException if the student is already registered for index indexNo
+	*/
 	public void register(Student student, int indexNo) throws AppException {
-		// check if student is already registered
-		// MUST DO ON thurs IF NOT NO SLEEP
 
 		if (findStudent(student) != null) {
 			throw new AppException(String.format("Already registered for %d in %s", indexNo, this));
 		}
 
 		Index index = getIndex(indexNo);
-		// add student to the specific course index of studentList
-		index.addStudent(student); // throw AppException if there is no vacancy in index class
+		index.addStudent(student); 
 	}
 
+	/** 
+	* Drops the student from the target index.
+	* @param student	The student that wants to drop the index.
+	* @return the current index if the dropping of index is successful.
+	* @throws AppException if the student is not in the index that the student wants to drop.
+	*/
 	public Index drop(Student student) throws AppException {
 		Index index = getStudentIndex(student);
-
 		index.removeStudent(student);
 		return index;
 	}
 
+	/** 
+	* Swop indexNoA of StudentA with indexNoB of StudentB
+	* @param studentA		The first student that wants to swop index
+	* @param studentB 		The second student that wants to swop index
+	* @param indexNoA		The index that the first student wants to swop
+	* @param indexNoB 		The other index that the second student wants to swop
+	* @throws AppException 	If indexNoA and indexNoB is the same index
+	* @throws AppException 	If studentA is not enrolled in indexNoA
+	* @throws AppException 	If studentB is not enrolled in indexNoB
+	*/
 	public void swopIndex(Student studentA, int indexNoA, Student studentB, int indexNoB) throws AppException {
-		// check whether both student has index
-		// MUST DO ON thurs IF NOT NO SLEEP (sleeping bag?)
 		
 		if (indexNoA == indexNoB) {
 			throw new AppException("Both indexes must be different");
@@ -146,12 +253,20 @@ public class Course extends Entity {
 			throw new AppException(String.format("%s is not enrolled in index %d", studentB.getMatricNo(), indexNoB));
 		}
 
-		indexA.removeStudent(studentA, false); // false to prevent waitlist from triggering
+		indexA.removeStudent(studentA, false); 
 		indexB.removeStudent(studentB, false);
 		indexA.addStudent(studentB);
 		indexB.addStudent(studentA);
 	}
 
+	/** 
+	* This method changes the student's index from the current index to the targeted index of targetIndexNo
+	* @param student		The student that wants to change index
+	* @param targetIndexNo 	The index number that the student wants to change to
+	* @throws AppException 	If the student is not registered in the current index
+	* @throws AppException 	If targetIndexNo is not found
+	* @throws AppException 	If targetIndexNo of the student has no more vacancies.
+	*/
 	public void changeIndex(Student student, int targetIndexNo) throws AppException {
 		Index indexCur = getStudentIndex(student);
 		Index indexTarget = getIndex(targetIndexNo);
@@ -166,6 +281,10 @@ public class Course extends Entity {
 		indexTarget.addStudent(student);
 	}
 
+	/** 
+	* Changes the course code name to a string format
+	* @return the course code as a string
+	*/
 	public String toString() {
 		return String.format("%s (%s)", name, courseCode);
 	}
