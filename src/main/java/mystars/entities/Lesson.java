79ec67c7cf3b774c4135edd1a60bee62c67771db
Justin Ong <justin.ong.97@gmail.com>
2020-11-16 11:36:53 +0800

    Refactor javadocs

--- src/main/java/mystars/entities/Lesson.java
+++ src/main/java/mystars/entities/Lesson.java
@@ -7,8 +7,7 @@ import mystars.enums.*;
 /**
  * <h1>Class: Lesson</h1>
  *
- * This Lesson class defines the blueprint of a lesson property.
- * It also checks instances where two objects of a different lesson clashes.
+ * Represents a Lesson, eg a Tutorial held on Monday between 0830-0930
  *
  */
 public class Lesson implements Serializable {
@@ -16,7 +15,7 @@ public class Lesson implements Serializable {
 	 * Serialization of the course ID
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	/**
 	 * Index that the lesson belongs to.
 	 */
@@ -38,36 +37,42 @@ public class Lesson implements Serializable {
 	private String location;
 
 	/**
-	 * Group number assigned to the particular lesson (applicable for tutorials & labs).
+	 * Group number assigned to the particular lesson (applicable for tutorials &
+	 * labs).
 	 */
 	private String groupNo;
 
 	/**
-	 * Specific weeks the lesson will be conducted.
+	 * Specific weeks the lesson will be conducted. boolean[13] representing whether
+	 * the lesson is held in that particular week eg week[0] == false signifies that
+	 * this lesson is not held in week 1
 	 */
 	private boolean[] weeks;
 
 	/**
-	 * Starting time for the lesson.
+	 * Starting time of the Lesson, from 0-31 where 0 represents 0800 and 31
+	 * represents 2330 respectively. Must be smaller than {@code endPeriod} (ie the
+	 * lesson must start before it can end)
 	 */
 	private int startPeriod;
 
 	/**
-	 * Ending time for the lesson.
+	 * Ending time for the lesson. See description for {@code startPeriod}. Must be
+	 * larger than {@code startPeriod}
 	 */
 	private int endPeriod;
 
 	/**
-	 * Constructor for Lesson. This class is responsible for the creation of a lesson object.
+	 * Creates new Lesson.
 	 * 
-	 * @param index      	The Lesson's index.
-	 * @param lessonType 	The Lesson's Type.
-	 * @param day     		The Lesson's day.
-	 * @param groupNo   	The Lesson's group number.
-	 * @param location     	The Lesson's location.
-	 * @param weeks    		The Lesson's week.
-	 * @param startPeriod	The Lesson's start period.
-	 * @param endPeriod	   	The Lesson's end period.
+	 * @param index       The Lesson's index.
+	 * @param lessonType  The Lesson's Type.
+	 * @param day         The Lesson's day.
+	 * @param groupNo     The Lesson's group number.
+	 * @param location    The Lesson's location.
+	 * @param weeks       The Lesson's week.
+	 * @param startPeriod The Lesson's start period.
+	 * @param endPeriod   The Lesson's end period.
 	 */
 	public Lesson(Index index, LessonType lessonType, Day day, String groupNo, String location, boolean[] weeks,
 			int startPeriod, int endPeriod) {
@@ -82,29 +87,31 @@ public class Lesson implements Serializable {
 	}
 
 	/**
-	 * Check for clashes between lessons.
+	 * Check for clashes between the current instance and {@code lesson}
 	 * 
-	 * @return true if there are clashes and false if there are none.
+	 * @param lesson Another lesson to compare against
+	 * @return {@code true} if there are clashes
+	 * @return {@code false} otherwise
 	 */
-	public boolean clashesWith(Lesson l) {
+	public boolean clashesWith(Lesson lesson) {
 		// loop through every week.
 		for (int week = 0; week < weeks.length; week++) {
 			// compare both lessons' week
-			if (this.weeks[week] && l.weeks[week]) {
+			if (this.weeks[week] && lesson.weeks[week]) {
 				// if both lessons occur on the same week, compare the day.
-				if (this.day == l.day) {
-					// if both lessons occur on the same week & day, compare the period they are conducted.
-					if (
-							(this.startPeriod >= l.startPeriod && this.startPeriod <= l.endPeriod) ||
-							(l.startPeriod >= this.startPeriod && l.startPeriod <= this.endPeriod)
-					) {
-						// if both lessons occur on the same week & day & overlaps in period, return true.		
+				if (this.day == lesson.day) {
+					// if both lessons occur on the same week & day, compare the period they are
+					// conducted.
+					if ((this.startPeriod >= lesson.startPeriod && this.startPeriod <= lesson.endPeriod)
+							|| (lesson.startPeriod >= this.startPeriod && lesson.startPeriod <= this.endPeriod)) {
+						// if both lessons occur on the same week & day & overlaps in period, return
+						// true.
 						return true;
 					}
 				}
 			}
 		}
-		
+
 		// if there is no conclusive clashes, return false.
 		return false;
 	}
