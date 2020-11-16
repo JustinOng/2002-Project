291b0a0af9907b7e76fb43acc3174144159f570f
Justin Ong <justin.ong.97@gmail.com>
2020-11-16 08:28:53 +0800

    Refactor javadocs

--- src/main/java/mystars/controllers/CourseController.java
+++ src/main/java/mystars/controllers/CourseController.java
@@ -20,7 +20,7 @@ import mystars.exceptions.AppException;
 
 public class CourseController {
 	/**
-	 * This method creates a new course.
+	 * Creates a new course.
 	 * 
 	 * @param name       The course's name.
 	 * @param courseCode The course's code.
@@ -29,16 +29,16 @@ public class CourseController {
 	 */
 
 	public void createCourse(String name, String courseCode, School school) throws AppException {
-		Course course = new Course(name, courseCode, school);
+		new Course(name, courseCode, school);
 	}
 
 	/**
-	 * This method creates a new index.
+	 * Creates a new index for the course identified by {@code courseCode}.
 	 * 
-	 * @param courseCode  The index course's code.
-	 * @param indexNo     The index no.
-	 * @param maxEnrolled The max enrolled of the course index.
-	 * @throws AppException If courseCode is not found.
+	 * @param courseCode  Identifier of Course
+	 * @param indexNo     Index number of the new index
+	 * @param maxEnrolled The maximum number of students enrolled in the new index
+	 * @throws AppException if no course identified by {@code courseCode} is found
 	 */
 
 	public void createIndex(String courseCode, int indexNo, int maxEnrolled) throws AppException {
@@ -46,29 +46,50 @@ public class CourseController {
 		c.createIndex(indexNo, maxEnrolled);
 	}
 
+	/**
+	 * Returns a list of all courses
+	 * 
+	 * @return list of all courses
+	 */
 	public List<Course> getAllCourses() {
 		return Course.getAllCourses();
 	}
 
+	/**
+	 * Returns a list of all indexes belonging to the course identified by
+	 * {@code courseCode}
+	 * 
+	 * @param courseCode Identifier of Course
+	 * @return list of indexes
+	 * @throws AppException if no course identified by {@code courseCode} is found
+	 */
 	public List<Index> getAllIndexes(String courseCode) throws AppException {
 		Course course = Course.getCourse(courseCode);
 		return course.getIndexes();
 	}
 
 	/**
-	 * This method creates a new lesson.
+	 * Creates a new lesson on the index and course identified by {@code indexNo}
+	 * and {@code courseCode} respectively
 	 * 
-	 * @param courseCode  The lesson's course code.
-	 * @param indexNo     The lesson's index no.
-	 * @param lessonType  The lesson type of the lesson.
-	 * @param day         The lesson's day.
-	 * @param Location    The lesson's location.
-	 * @param groupNo     The lesson's group number.
-	 * @param week        The lesson's week.
-	 * @param startPeriod The lesson's start period.
-	 * @param endPeriod   The lesson's end period.
-	 * @throws AppException If courseCode is not found.
-	 * @throws AppException If indexNo is not found.
+	 * @param courseCode  Identifier of Course
+	 * @param indexNo     Identifier of Index
+	 * @param lessonType  Lesson type of the new lesson
+	 * @param day         Day that the new Lesson is held on
+	 * @param Location    Location where the Lesson is held
+	 * @param groupNo     Group number of the lesson eg SEP1
+	 * @param week        boolean[13] representing whether the lesson is held in
+	 *                    that particular week eg week[0] == false signifies that
+	 *                    this lesson is not held in week 1
+	 * @param startPeriod Start period of the Lesson, from 0-31 where 0 represents
+	 *                    0800 and 31 represents 2330 respectively. Must be smaller
+	 *                    than {@code endPeriod} (ie the lesson must start before it
+	 *                    can end)
+	 * @param endPeriod   End period of the Lesson, see description in
+	 *                    {@code startPeriod}. Must be greater than
+	 *                    {@code startPeriod}.
+	 * @throws AppException if no course identified by {@code courseCode} is found
+	 * @throws AppException if no index identified by {@code indexNo} is found
 	 */
 
 	public void createLesson(String courseCode, int indexNo, LessonType lessonType, Day day, String location,
@@ -78,14 +99,16 @@ public class CourseController {
 	}
 
 	/**
-	 * This method register the student to a course.
+	 * Register a student for a particular index
 	 * 
-	 * @param student The student's.
-	 * @param indexNo The index no the student wants to register.
-	 * @throws AppException If indexNo is not found.
-	 * @throws AppException If index has already been registered.
-	 * @throws AppException If index clashes with another index.
-	 * @throws AppException If indexNo of the student has already been registered.
+	 * @param student Student to register
+	 * @param indexNo Identifier of Index
+	 * @throws AppException if no index identified by {@code indexNo} is found
+	 * @throws AppException if {@code student} has already been registered for the
+	 *                      index identified by {@code indexNo}
+	 * @throws AppException if the index identified by {@code indexNo} clashes with
+	 *                      other indexes that {@code student} is already registered
+	 *                      for
 	 */
 	public void registerCourse(Student student, int indexNo) throws AppException {
 		Index index = Index.getIndex(indexNo);
@@ -96,12 +119,13 @@ public class CourseController {
 	}
 
 	/**
-	 * This method drop the student from a course.
+	 * Drop a student from a particular course
 	 * 
-	 * @param student    The student who wants to drop the course.
-	 * @param courseCode The course that the student wants to drop.
-	 * @throws AppException If courseCode does not exist.
-	 * @throws AppException If student is not enrolled in the course.
+	 * @param student    Student to drop
+	 * @param courseCode Identifier of Course
+	 * @throws AppException if no course identified by {@code courseCode} is found
+	 * @throws AppException If {@code student} is not registered for the course
+	 *                      identified by {@code courseCode}
 	 * 
 	 */
 	public void dropCourse(Student student, String courseCode) throws AppException {
@@ -114,15 +138,17 @@ public class CourseController {
 	 * This method changes the student's index from the current index to the
 	 * targeted index of targetIndexNo
 	 * 
-	 * @param courseCode    The course's code.
-	 * @param student       The student that wants to change index.
-	 * @param targetIndexNo The index number that the student wants to change to.
-	 * @throws AppException If courseCode is not found.
-	 * @throws AppException If student is not enrolled in the course.
-	 * @throws AppException If targetIndexNo is not found.
-	 * @throws AppException If student's timetable cannot accommodate newIndex after
-	 *                      removing curIndex.
-	 * @throws AppException If targetIndexNo of the student has no more vacancies.
+	 * @param courseCode    Identifier of Course
+	 * @param student       Student to change index
+	 * @param targetIndexNo The index number that the student wants to change to
+	 * @throws AppException if no course identified by {@code courseCode}
+	 * @throws AppException if student is not enrolled in the course identified by
+	 *                      {@code courseCode}
+	 * @throws AppException if no index identified by {@code targetIndexNo} is found
+	 * @throws AppException if the index identified by {@code targetIndexNo} has no
+	 *                      more vacancies
+	 * @throws AppException if adding the index identified by {@code targetIndexNo}
+	 *                      after removing the current index would result in a clash
 	 * 
 	 */
 	public void changeIndex(String courseCode, Student student, int targetIndexNo) throws AppException {
@@ -139,21 +165,19 @@ public class CourseController {
 	/**
 	 * This method allow the student to swop the course index with another student.
 	 * 
-	 * @param studentA The studentA's.
-	 * @param indexNoA The studentA's index.
-	 * @param studentB The studentB's.
-	 * @param indexNoB The studentB's index.
-	 * @throws AppException If studentA is studentB.
-	 * @throws AppException If indexNoA is not found.
-	 * @throws AppException If indexNoB is not found.
-	 * @throws AppException If indexA.getCourse() is not same as indexB.getCourse()
-	 * @throws AppException If studentA's timetable cannot accommodate indexNoB
-	 *                      after removing indexA.
-	 * @throws AppException If studentB's timetable cannot accommodate indexNoA
-	 *                      after removing indexB.
-	 * @throws AppException If indexNoA and indexNoB is the same index.
-	 * @throws AppException If studentA is not enrolled in indexNoA.
-	 * @throws AppException If studentB is not enrolled in indexNoB.
+	 * @param studentA Student A
+	 * @param indexNoA Identifier of Index which Student A would like to swap
+	 * @param studentB Student B
+	 * @param indexNoB Identifier of Index which Student B would like to swap
+	 * @throws AppException if {@code studentA} is the same as {@code studentB} or
+	 *                      {@code indexNoA} is the same as {@code indexNoB}
+	 * @throws AppException if {@code indexNoA} or {@code indexNoB} do not identify
+	 *                      valid indexes
+	 * @throws AppException if {@code indexNoA} or {@code indexNoB} belong to the
+	 *                      same course
+	 * @throws AppException if {@code studentA} or {@code studentB} is not enrolled
+	 *                      in the indexes they would like to swap
+	 * @throws AppException if swopping the index will result in a timetable clash
 	 * 
 	 */
 
@@ -178,6 +202,12 @@ public class CourseController {
 		studentB.getTimetable().addIndex(indexA);
 	}
 
+	/**
+	 * Returns the indexes that {@code student} is enrolled in
+	 * 
+	 * @param student
+	 * @return list of indexes that {@code student} is enrolled in
+	 */
 	public List<Index> getStudentIndexes(Student student) {
 		List<Index> indexes = new ArrayList<Index>();
 
@@ -188,18 +218,47 @@ public class CourseController {
 		return indexes;
 	}
 
+	/**
+	 * Returns the indexes belonging to the course identified by {@code courseCode}
+	 * 
+	 * @param courseCode Identifier of Course
+	 * @return list of indexes belonging to the course identified by
+	 *         {@code courseCode}
+	 * @throws AppException if no course identified by {@code courseCode} is found
+	 */
 	public List<Index> getCourseIndexes(String courseCode) throws AppException {
 		return Course.getCourse(courseCode).getIndexes();
 	}
 
+	/**
+	 * Returns the students enrolled in the course identified by {@code courseCode}
+	 * 
+	 * @param courseCode Identifier of Course
+	 * @return list of students enrolled in the course identified by
+	 *         {@code courseCode}
+	 * @throws AppException if no course identified by {@code courseCode} is found
+	 */
 	public List<Student> getStudentsByCourse(String courseCode) throws AppException {
 		return Course.getCourse(courseCode).getStudentList();
 	}
 
+	/**
+	 * Returns the students enrolled in the index identified by {@code indexNo}
+	 * 
+	 * @param indexNo Identifier of Index
+	 * @return list of students enrolled in the index identified by {@code indexNo}
+	 * @throws AppException if no index identified by {@code indexNo} is found
+	 */
 	public List<Student> getStudentsByIndex(int indexNo) throws AppException {
 		return Index.getIndex(indexNo).getStudentList();
 	}
 
+	/**
+	 * Returns the number of vacancies in the index identified by {@code indexNo}
+	 * @param indexNo Identifier of Index
+	 * @return number of vacancies in the index identified by {@code indexNo}
+	 * @throws AppException if no index identified by {@code indexNo} is found
+	 */
 	public int getVacancies(int indexNo) throws AppException {
 		return Index.getIndex(indexNo).getVacancies();
 	}
