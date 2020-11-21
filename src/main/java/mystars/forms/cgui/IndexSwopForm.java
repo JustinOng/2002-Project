37ab0197d53322562879229aecf0f6fed15f2248
Justin Ong <justin.ong.97@gmail.com>
2020-11-21 12:11:48 +0800

    Add validation to index inputs on IndexSwopForm

--- src/main/java/mystars/forms/cgui/IndexSwopForm.java
+++ src/main/java/mystars/forms/cgui/IndexSwopForm.java
@@ -5,14 +5,15 @@ import java.util.regex.Pattern;
 
 import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.gui2.*;
+import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 
 import mystars.forms.*;
 
 /**
  * <h1>Class: IndexSwopForm</h1>
  * 
- * This class manages the user interface for the form for swopping of 
- * course indexes between two students.
+ * This class manages the user interface for the form for swopping of course
+ * indexes between two students.
  */
 public class IndexSwopForm {
 	/**
@@ -21,15 +22,15 @@ public class IndexSwopForm {
 	private IndexSwopResponse response;
 
 	/**
-	 * Sets the parameters for the graphical user interface students will
-	 * use when swopping their course index with another student.
+	 * Sets the parameters for the graphical user interface students will use when
+	 * swopping their course index with another student.
 	 * 
-	 * @param gui	The graphical user interface object.
-	 * @return		The index swopping form response object.
+	 * @param gui The graphical user interface object.
+	 * @return The index swopping form response object.
 	 */
 	public IndexSwopResponse getResponse(MultiWindowTextGUI gui) {
 		final AbstractWindow window = new BasicWindow();
-		
+
 		response = null;
 
 		// Create new Jpanel object and set the layout as a grid.
@@ -37,9 +38,6 @@ public class IndexSwopForm {
 		panel.setLayoutManager(new GridLayout(2));
 
 		// Insert textboxes, labels and buttons as required.
-		final Label status = new Label("");
-		panel.addComponent(status, GridLayout.createHorizontallyFilledLayoutData(2));
-
 		panel.addComponent(new Label("Your Index Number:"));
 		final TextBox indexAInput = new TextBox().addTo(panel);
 		indexAInput.setValidationPattern(Pattern.compile("^[0-9]+$"));
@@ -59,8 +57,16 @@ public class IndexSwopForm {
 
 		new Button("Swop", new Runnable() {
 			public void run() {
-				response = new IndexSwopResponse(Integer.parseInt(indexAInput.getText()),
-						Integer.parseInt(indexBInput.getText()), usernameInput.getText(), passwordInput.getText());
+				int indexA, indexB;
+				try {
+					indexA = Integer.parseInt(indexAInput.getText());
+					indexB = Integer.parseInt(indexBInput.getText());
+				} catch (NumberFormatException e) {
+					MessageDialog.showMessageDialog(gui, "Error", "Error parsing number");
+					return;
+				}
+
+				response = new IndexSwopResponse(indexA, indexB, usernameInput.getText(), passwordInput.getText());
 				window.close();
 			}
 		}).addTo(panel);
